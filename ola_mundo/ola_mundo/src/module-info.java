4126dad7099ac8d2148987b9c4317f9993617d8f
raphoz <82879206+raphoz@users.noreply.github.com>
2023-03-24 11:24:28 -0300

    upload ola mundi
    ola mundo

---
+++ ola_mundo/ola_mundo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ilan
+ *
+ */
+module ola_mundo {
+}
---
+++ ola_mundo/ola_mundo/src/ola_mundo/Ola.java
@@ -0,0 +1,10 @@
+package ola_mundo;
+
+public class Ola {
+
+	public static void main(String[] args) {
+		System.out.println("Olá, mundo do github!");
+
+	}
+
+}
